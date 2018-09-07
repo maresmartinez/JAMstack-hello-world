265a3e1ed85cb19d0a3081b6a93a3ce13e4bb64d
Mariel Martinez <mcortez.martinez@gmail.com>
2018-09-07 11:58:41 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Mariel "Mares" Martinez</h1>
+    <h3>Computer Programming Student</h3>
+    <p>Hi, all! I'm a student at Sheridan College studying towards a Computer Programmer Diploma. I'm looking to learn more skills outside of what we cover in school, and this Hackathon is my first step into that. I'm excited to compete!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
